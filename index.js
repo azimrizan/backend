567beb66200d8bcdc1c6f12ec32c0197295bfc61
Azim Rizan <123349236+azimrizan@users.noreply.github.com>
2024-08-26 03:15:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 const cors = require("cors");
 
 const app = express();
@@ -7,12 +6,6 @@ const app = express();
 // Apply CORS middleware globally
 app.use(cors());
 
-=======
-const cors = require("cors"); // Import the cors module
-const app = express();
-
-app.use(cors()); // Use the cors middleware
->>>>>>> 7e54666 (Your commit message)
 app.use(express.json());
 
 app
@@ -21,39 +14,34 @@ app
     res.status(200).json({ operation_code: 1 });
   })
   .post((req, res) => {
-    try {
-      const data = req.body.data || [];
-      const numbers = [];
-      const alphabets = [];
-      let highest_lowercase_alphabet = "";
+    const data = req.body.data || [];
+    const numbers = [];
+    const alphabets = [];
+    let highest_alphabet = "";
 
-      for (const item of data) {
-        if (!isNaN(item)) {
-          numbers.push(item);
-        } else if (item.length === 1 && isNaN(item)) {
-          alphabets.push(item);
-          if (
-            !highest_lowercase_alphabet ||
-            (item === item.toLowerCase() && item > highest_lowercase_alphabet)
-          ) {
-            highest_lowercase_alphabet = item;
-          }
+    for (const item of data) {
+      if (!isNaN(item)) {
+        numbers.push(item);
+      } else if (item.length === 1 && isNaN(item)) {
+        alphabets.push(item);
+        if (
+          !highest_alphabet ||
+          item.toUpperCase() > highest_alphabet.toUpperCase()
+        ) {
+          highest_alphabet = item;
         }
       }
-
-      res.json({
-        is_success: true,
-        user_id: "azim_rizan_15032003",
-        email: "azim.21bce8448@vitapstudent.ac.in",
-        roll_number: "21BCE8448",
-        numbers: numbers,
-        alphabets: alphabets,
-        highest_lowercase_alphabet: highest_lowercase_alphabet ? [highest_lowercase_alphabet] : [],
-      });
-    } catch (error) {
-      console.error("Error processing request:", error);
-      res.status(500).json({ is_success: false, message: "Internal Server Error" });
     }
+
+    res.json({
+      is_success: true,
+      user_id: "azim_rizan",
+      email: "azim.21bce8448@vitapstudent.ac.in",
+      roll_number: "21BCE8448",
+      numbers: numbers,
+      alphabets: alphabets,
+      highest_alphabet: highest_alphabet ? [highest_alphabet] : [],
+    });
   });
 
 const port = process.env.PORT || 3000;
